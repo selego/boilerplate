246658a0c0a7e88bb23261bbcfde4aac53f06db9
Jawad Ul Hassan <janialbela@gmail.com>
2020-11-02 14:12:18 +0500

    ml

--- admin/src/app.js
+++ admin/src/app.js
@@ -52,7 +52,7 @@ export default () => {
         <div className="main">
           {user && <Drawer />}
           {user && <Header />}
-          <div style={{ height: "100%", marginLeft: 160 }}>
+          <div style={{ height: "100%", marginLeft: user ? 160 : 0 }}>
             <Switch>
               <Route path="/auth" component={Auth} />
               <RestrictedRoute path="/account" component={Account} />
