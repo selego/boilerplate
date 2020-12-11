671b3bbd23368ed6b52d32463644a04d31c2de25
Jawad Ul Hassan <janialbela@gmail.com>
2020-12-11 12:49:05 +0500

    up

--- README.md
+++ README.md
@@ -2,15 +2,15 @@
 
 ## Setup admin
 
-- cd admin 
+- cd admin
 - Run `npm i`
 - Run `npm run dev`
-- Set project variables in config.js file
+- Set variables such as `apiURL`, `S3PREFIX` and `SENTRY_URL` in `config.js` file
 - Signup as new user and continue
 
 ## Setup api
 
-- cd api 
-- Run `npm i` 
+- cd api
+- Run `npm i`
 - Run `npm run dev`
-- Set project variables in .env (create new if not found) and config.js file
+- Set project variables in `.env` (create new if not found) and `config.js` file
--- admin/src/config.js
+++ admin/src/config.js
@@ -1,11 +1,19 @@
 const forceProd = false;
 
-let apiURL = "https://api.maakyo.com";
-if (window.location.href.indexOf("localhost") !== -1 || window.location.href.indexOf("127.0.0.1") !== -1) {
-  apiURL = "http://localhost:3000";
-}
+const environment = getEnvironment();
+
+let apiURL = "https://api.selego.com";
+
+if (environment === "staging") apiURL = "https://api-staging.selego.fr";
+if (environment === "development") apiURL = "http://localhost:3000";
 
 const S3PREFIX = "https://datadvise.s3.eu-west-3.amazonaws.com/app";
 const SENTRY_URL = "";
 
-export { apiURL, S3PREFIX, SENTRY_URL };
+function getEnvironment() {
+  if (window.location.href.indexOf("app-staging") !== -1) return "staging";
+  if (window.location.href.indexOf("localhost") !== -1 || window.location.href.indexOf("127.0.0.1") !== -1) return "development";
+  return "production";
+}
+
+export { apiURL, S3PREFIX, SENTRY_URL, environment };
